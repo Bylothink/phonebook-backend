61e574394125db88303c39b770ad85355317ac15
Matteo Bilotta <me@byloth.net>
2022-05-26 00:18:31 +0200

    [ADD] Aggiunta compatibilità CORS.

--- src/phonebook/app.py
+++ src/phonebook/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from .config import config
 
@@ -6,3 +7,4 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = config.database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+CORS(app)
